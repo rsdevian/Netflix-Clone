f2f2b1f085560b86d94f9fe9e308982d9c5f4fae
Vaishnav Nishanth AV <vaishnavnishanth3@gmail.com>
2024-06-30 20:23:14 +0530

    Adding Genres

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <h1>Hey! Let's Build Netflix Front-end Today!!</h1>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixoriginals}/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaryMovies}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from './axios.js';
 import "./Row.css";
 
@@ -6,31 +6,35 @@ const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({title,fetchUrl}) {
     const [movies, setMovies] = useState([]);
-    
+        
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             return request;
         }
-        fetchData();
+            fetchData();
     },[fetchUrl])
 
     console.table(movies)
 
     return (
-    <div className='row'>
-        <h1>{title}</h1>
-        <div className='row__posters'>
-            {
-                movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-                ))
-            }
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row__posters'>
+                {
+                    movies.map(movie => (
+                        <img
+                            key={movie.id} 
+                            className='row__poster'
+                            src={`${base_url}${movie.poster_path}`} 
+                            alt={movie.name}
+                        />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  )
-}
+    )}
 
 export default Row;
 
