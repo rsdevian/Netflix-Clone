5d42eb77b18b7d28e0992af26041d2763bf7f40e
Vaishnav Nishanth AV <vaishnavnishanth3@gmail.com>
2024-07-01 15:21:38 +0530

    Added Playbacks

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,19 @@
+import React from "react";
+import "./Nav.css"
+
 export default function Nav() {
     return (
-        <div></div>
+        <div className="nav">
+            <img
+                className="nav__logo"
+                src="https://wallpaperaccess.com/full/6075689.png"    
+                alt="Netflix Logo"
+            />
+            <img
+                className="profile-image"
+                src="https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg"
+                alt="Netflix Profile Logo"
+            />
+        </div>
     )
 }
--- src/Row.js
+++ src/Row.js
@@ -1,37 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios.js';
-import "./Row.css";
+import './Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
-const base_url = "https://image.tmdb.org/t/p/original/"
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({title,fetchUrl}) {
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
-        
+    const [trailerUrl, setTrailerUrl] = useState('');
+
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
+            try {
+                const request = await axios.get(fetchUrl);
+                setMovies(request.data.results);
+                return request;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
-            fetchData();
-    },[fetchUrl])
+        fetchData();
+    }, [fetchUrl]);
+
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.title || movie?.name || '')
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch((error) => console.log(error));
+        }
+    };
 
     return (
-        <div className='row'>
+        <div className="row">
             <h2>{title}</h2>
-            <div className='row__posters'>
-                {
-                    movies.map(movie => (
-                        <img
-                            key={movie.id} 
-                            className='row__poster'
-                            src={`${base_url}${movie.poster_path}`} 
-                            alt={movie.name}
-                        />
-                    ))
-                }
+            <div className="row__posters">
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        onClick={() => handleClick(movie)}
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.title || movie.name}
+                    />
+                ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
-    )}
+    );
+}
 
 export default Row;
